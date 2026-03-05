4f2567c3dfb3c07bda99fcf95334f7fdfd641d8d
Frederico <frednunnenkamp@gmail.com>
2026-03-04 21:29:55 -0300

    Improve updater: lightweight polling + atomic deploy

--- git_site_host/rootfs/run.sh
+++ git_site_host/rootfs/run.sh
@@ -2,12 +2,5 @@
 set -euo pipefail
 
 /usr/local/bin/updater.sh &
-UPDATER_PID=$!
-
-WEBHOOK_ENABLED="$(jq -r '.webhook_enabled // false' /data/options.json)"
-if [[ "$WEBHOOK_ENABLED" == "true" ]]; then
-  export UPDATER_PID
-  /usr/local/bin/webhook_server.py &
-fi
 
 exec nginx -g "daemon off;"
